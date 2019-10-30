83e8dc611d9ea01d8552d3f5e32e2b40a0f7a0ea
Nima Shoghi <nimashoghi@gmail.com>
2019-10-30 11:11:45 -0400

    Only add code=code-insiders alias if code-insiders is installed

--- fish/alpine/3.10/config.fish
+++ fish/alpine/3.10/config.fish
@@ -2,4 +2,6 @@ function fish_user_key_bindings
     bind --key nul complete-and-search
 end
 
-alias code=code-insiders
+if type -q code-insiders; and not type -q code
+    alias code=code-insiders
+end
