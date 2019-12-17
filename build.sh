73bb48cdd1b0b74f0ef5955aaafecdd8ae3420b6
Nima Shoghi <nimashoghi@gmail.com>
2019-12-17 17:28:55 -0500

    Set latest node image

--- build.sh
+++ build.sh
@@ -57,16 +57,19 @@ docker tag "$DOCKER_USERNAME/node:slim-current-$TRAVIS_TAG" "$DOCKER_USERNAME/no
 docker tag "$DOCKER_USERNAME/node:slim-current-$TRAVIS_TAG" "$DOCKER_USERNAME/node:current"
 docker push $DOCKER_USERNAME/node:slim-current-$TRAVIS_TAG
 docker push $DOCKER_USERNAME/node:slim-current
+docker push $DOCKER_USERNAME/node:current
 docker build --rm -f "./node/slim/latest/Dockerfile" -t "$DOCKER_USERNAME/node:slim-latest-$TRAVIS_TAG" "./node/slim/latest"
 docker tag "$DOCKER_USERNAME/node:slim-latest-$TRAVIS_TAG" "$DOCKER_USERNAME/node:slim-latest"
 docker tag "$DOCKER_USERNAME/node:slim-latest-$TRAVIS_TAG" "$DOCKER_USERNAME/node:latest"
 docker push $DOCKER_USERNAME/node:slim-latest-$TRAVIS_TAG
 docker push $DOCKER_USERNAME/node:slim-latest
+docker push $DOCKER_USERNAME/node:latest
 docker build --rm -f "./node/slim/lts/Dockerfile" -t "$DOCKER_USERNAME/node:slim-lts-$TRAVIS_TAG" "./node/slim/lts"
 docker tag "$DOCKER_USERNAME/node:slim-lts-$TRAVIS_TAG" "$DOCKER_USERNAME/node:slim-lts"
 docker tag "$DOCKER_USERNAME/node:slim-lts-$TRAVIS_TAG" "$DOCKER_USERNAME/node:lts"
 docker push $DOCKER_USERNAME/node:slim-lts-$TRAVIS_TAG
 docker push $DOCKER_USERNAME/node:slim-lts
+docker push $DOCKER_USERNAME/node:lts
 
 # python
 docker build --rm -f "./python/${TARGET_ENVIRONMENT:-slim}/2.7/Dockerfile" -t "$DOCKER_USERNAME/python:2.7-$TRAVIS_TAG" "./python/${TARGET_ENVIRONMENT:-slim}/2.7"
