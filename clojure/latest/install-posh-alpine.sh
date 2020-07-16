7f09d7ce5ac735f8b1c583f0848e4f0d84136dfb
Nima Shoghi <nimashoghi@gmail.com>
2020-07-16 18:59:18 -0400

    Update to pwsh 7.0.3

--- clojure/latest/install-posh-alpine.sh
+++ clojure/latest/install-posh-alpine.sh
@@ -21,7 +21,7 @@ apk -X https://dl-cdn.alpinelinux.org/alpine/edge/main add --no-cache \
     lttng-ust
 
 # Download the powershell '.tar.gz' archive
-curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.0.2/powershell-7.0.2-linux-alpine-x64.tar.gz -o /tmp/powershell.tar.gz
+curl -L https://github.com/PowerShell/PowerShell/releases/download/v7.0.3/powershell-7.0.3-linux-alpine-x64.tar.gz -o /tmp/powershell.tar.gz
 
 # Create the target folder where powershell will be placed
 mkdir -p /opt/microsoft/powershell/7
